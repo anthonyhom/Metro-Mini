ffc1de09f6bf938549d37bdd75147bf201e3cf1e
Jonathan Huang <Jonathan Huang>
2017-06-01 19:00:46 -0400

    Sketching game elements

--- Game/Game.pde
+++ Game/Game.pde
@@ -1,14 +1,59 @@
-PImage img;
+PImage image;
+GameClass game;
 
 void setup() {
     size(1920, 1080);
-    img = loadImage("../Maps/map_00.png");
+    game = new GameClass();
+    image = loadImage("../Maps/Map-London.png");
 }
 
 void draw() {
     background(0);
-    image(img, 0, 0);
+    image(image, 0, 0);
+    textSize(32);
+    text(game.proc + "", 1920 / 2, 1080 / 2);
+    fill(0, 0, 0);
 }
+
+void update() {
+
+}
+
+
+
+class GameClass {
+
+    int proc;
+    boolean paused;
+
+    GameClass() {
+        this.proc = 0;
+        this.paused = false;
+    }
+
+    void play() {
+        this.paused = false;
+        while (! this.paused) {
+            try {
+                Thread.sleep(10);
+                this.proc += 1;
+                redraw();
+            }
+            catch (InterruptedException e) { }
+        }
+    }
+
+    void pause() {
+        this.paused = true;
+    }
+
+    int getProc() {
+        return this.proc;
+    }
+
+}
+
+
 
 class Passenger {
 
@@ -20,6 +65,16 @@ class Passenger {
 
 }
 
+
+
 class Station {
-    
+
+    int x, y, id;
+    String filename;
+
+    Station(int x, int y, String shape, int id) {
+        this.x = x;
+        this.y = y;
+    }
+
 }
--- Game/build/source/Game.java
+++ Game/build/source/Game.java
@@ -14,25 +14,84 @@ import java.io.IOException;
 
 public class Game extends PApplet {
 
-PImage img;
+PImage image;
+GameClass game;
 
 public void setup() {
-  
-  img = loadImage("../Maps/map_00.png");
+    
+    game = new GameClass();
+    image = loadImage("../Maps/Map-London.png");
 }
 
 public void draw() {
-  background(0);
-  image(img, 0, 0);
+    background(0);
+    image(image, 0, 0);
+    textSize(32);
+    text(game.proc + "", 1920 / 2, 1080 / 2);
+    fill(0, 0, 0);
 }
+
+public void update() {
+
+}
+
+
+
+class GameClass {
+
+    int proc;
+    boolean paused;
+
+    GameClass() {
+        this.proc = 0;
+        this.paused = false;
+    }
+
+    public void play() {
+        this.paused = false;
+        while (! this.paused) {
+            try {
+                Thread.sleep(10);
+                this.proc += 1;
+                redraw();
+            }
+            catch (InterruptedException e) { }
+        }
+    }
+
+    public void pause() {
+        this.paused = true;
+    }
+
+    public int getProc() {
+        return this.proc;
+    }
+
+}
+
+
 
 class Passenger {
 
-  int patience;
+    int patience;
 
-  Passenger(int patience) {
-    this.patience = patience;
-  }
+    Passenger(int patience) {
+        this.patience = patience;
+    }
+
+}
+
+
+
+class Station {
+
+    int x, y, id;
+    String filename;
+
+    Station(int x, int y, String shape, int id) {
+        this.x = x;
+        this.y = y;
+    }
 
 }
   public void settings() {  size(1920, 1080); }
